7db924b31f4c60bec86f4a5abdc0f99d2aea626c
Kristian Numme <95479930+Kristiangit@users.noreply.github.com>
2023-09-28 09:58:57 +0200

    movement og rotasjon bra

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -31,7 +31,7 @@ public class CameraMovement : MonoBehaviour
 
         // Camera.transform.rotation = Quaternion.Euler(LookX, LookY*-1, 0);
 
-        Camera.transform.Rotate(LookX*-1, LookY, 0, Space.World);
+        Camera.transform.Rotate(LookX*-1, LookY, 0, Space.Self);
         
         Target.transform.rotation = Camera.transform.rotation;
     }
